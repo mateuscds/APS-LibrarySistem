fb5310f9a5cacc2b6486cfefe493e76f4aef324d
Matheus Albuquerque <mvca@cin.ufpe.br>
2022-08-15 13:48:01 -0300

    emprestimoService calls the external system

--- web/demo/src/main/java/com/example/demo/controllers/PagamentoController.java
+++ web/demo/src/main/java/com/example/demo/controllers/PagamentoController.java
@@ -4,6 +4,7 @@ import java.time.LocalDate;
 
 import javax.servlet.http.HttpSession;
 
+import org.hibernate.hql.internal.ast.tree.IdentNode;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -27,15 +28,18 @@ public class PagamentoController {
         Double valor = ChronoUnit.DAYS.between(LocalDate.now(), emp.getDataTerminoEmprestimo()) * 2.;
         ModelAndView mv = new ModelAndView("/pagamento/showPagamento");
         mv.addObject("valor", Math.abs(valor));
+        mv.addObject("idemprestimo", idEmprestimo);
         return mv;
     }
 
-    @PostMapping("/livros/pagamento")
-    public ModelAndView emitePagamento(String email, Double valor, HttpSession session) {
+    @PostMapping("/livros/pagamento/{idEmprestimo}")
+    public ModelAndView emitePagamento(@PathVariable Long idEmprestimo, String email, Double valor, HttpSession session) {
         boolean res = facade.emitePagamento(email, valor);
 
-        if (res == true)
+        if (res == true) {
+            facade.atualizaStatusEmprestimo(idEmprestimo);
             return new ModelAndView("/pagamento/pagamentoSucesso");
+        }
         else
             return new ModelAndView("/pagamento/pagamentoFalha");
     }
