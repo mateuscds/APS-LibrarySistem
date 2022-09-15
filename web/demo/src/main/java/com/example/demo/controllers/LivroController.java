9594fea657c16b18d2ad3fb58fde99cb6db9b46e
Matheus Albuquerque <mvca@cin.ufpe.br>
2022-09-15 00:52:28 -0300

    separated cadastro,editaLivro and editaQuantidade from livroController

--- web/demo/src/main/java/com/example/demo/controllers/LivroController.java
+++ web/demo/src/main/java/com/example/demo/controllers/LivroController.java
@@ -26,6 +26,7 @@ public class LivroController {
 
         List<Livro> livros = facade.buscarTodosLivros();
         ModelAndView mv;
+        // TODO Modificar quando mudar para sessão
         if (client.equals("funcionario")) {
             mv = new ModelAndView("livro/showFuncionario");
         } else {
@@ -35,50 +36,8 @@ public class LivroController {
         return mv;
     }   
 
-    @GetMapping("/livros/cadastrarlivro")
-    public ModelAndView showCadastroLivro(HttpSession session) {
-        return new ModelAndView("/livro/cadastroLivro");
-    }
-
-    @PostMapping("/livros/cadastrarlivro")
-    public ModelAndView cadastroLivro(String nome, String edicao, int quantidade, HttpSession session) {
-        String res = facade.cadastrarLivro(nome, edicao, quantidade);
-
-        if (res.equals("100")) {
-            return new ModelAndView("redirect:/funcionario");
-        } else {
-            ModelAndView mv = new ModelAndView("redirect:/livros/cadastrarlivro");
-            mv.addObject("error", "exist");
-            return mv;
-        }
-    }
-
-    @GetMapping("/livros/editalivro")
-    public ModelAndView editaLivro(HttpSession session) {
-        ModelAndView mv = new ModelAndView("/livro/editaLivro");
-        mv.addObject("livros", facade.buscarTodosLivros());
-        return mv;
-    }
-
-    @GetMapping("/livros/{livroId}/editaquantidade")
-    public ModelAndView showEditaQuantidadeLivro(@PathVariable Long livroId, HttpSession session) {
-        ModelAndView mv = new ModelAndView("/livro/editaQuantidadeLivro");
-        mv.addObject("livroindex", livroId);
-
-        return mv;
-    }
-
-    @PostMapping("/livros/{livroId}/editaquantidade")
-    public ModelAndView editaQuantidadeLivro(@PathVariable Long livroId, int quantidade, HttpSession session) {
-        System.out.println(livroId);
-        System.out.println(quantidade);
-        
-        facade.atualizarQuantidadeLivro(livroId, quantidade);
-        return new ModelAndView("redirect:/livros/editalivro");
-    }
-
     @GetMapping("/livros/{livroId}/deletar")
-    public ModelAndView editaQuantidadeLivro(@PathVariable Long livroId, HttpSession session) {
+    public ModelAndView deletarLivro(@PathVariable Long livroId, HttpSession session) {
 
         facade.deletarLivro(livroId);
         return new ModelAndView("redirect:/livros/editalivro");
